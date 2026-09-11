7bc18c9836ef0aac427ae6362b5dc263132e2a80
Leonardo Anjos <leodrums11@gmail.com>
2026-09-11 17:34:13 -0300

    favicon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -92,7 +92,11 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
         rel: "stylesheet",
         href: appCss,
       },
-      { rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
+      {
+        rel: "icon",
+        href: "/Screenshot%202026-09-11%200203101.png",
+        type: "image/png",
+      },
     ],
   }),
   shellComponent: RootShell,
